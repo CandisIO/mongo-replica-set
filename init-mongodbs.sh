c1a872ba1d837170b57e19916dde9df6fc04ef44
Bello Oladipupo <bellooladipupo41@gmail.com>
2022-09-29 15:40:01 +0100

    chore: log mongod command

--- init-mongodbs.sh
+++ init-mongodbs.sh
@@ -13,6 +13,9 @@ DB3_LOG_DIR="/var/log/mongodb3"
 REPLICA_SET="${REPLICA_SET_NAME:-rs0}"
 
 
+echo "mongod --dbpath ${DB1_DATA_DIR} --logpath ${DB1_LOG_DIR}/mongod.log --fork --port 27017 --bind_ip_all --replSet $REPLICA_SET $@"
 mongod --dbpath ${DB1_DATA_DIR} --logpath ${DB1_LOG_DIR}/mongod.log --fork --port 27017 --bind_ip_all --replSet $REPLICA_SET "$@"
+echo "mongod --dbpath ${DB2_DATA_DIR} --logpath ${DB2_LOG_DIR}/mongod.log --fork --port 27018 --bind_ip_all --replSet $REPLICA_SET $@"
 mongod --dbpath ${DB2_DATA_DIR} --logpath ${DB2_LOG_DIR}/mongod.log --fork --port 27018 --bind_ip_all --replSet $REPLICA_SET "$@"
+echo "mongod --dbpath ${DB3_DATA_DIR} --logpath ${DB3_LOG_DIR}/mongod.log --port 27019 --bind_ip_all --replSet $REPLICA_SET $@"
 mongod --dbpath ${DB3_DATA_DIR} --logpath ${DB3_LOG_DIR}/mongod.log --port 27019 --bind_ip_all --replSet $REPLICA_SET "$@"
